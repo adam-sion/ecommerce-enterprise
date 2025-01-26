6ae215437c50ab02b1d1b3e94c19224e7da36d34
adam-sion <adamsion74@gmail.com>
2025-01-26 15:47:59 +0200

    feat: init db user

---
+++ back/ecom-enterprise/src/main/java/adam/dev/ecom_enterprise/entity/User.java
@@ -0,0 +1,33 @@
+package adam.dev.ecom_enterprise.entity;
+
+import jakarta.persistence.*;
+import lombok.Data;
+import lombok.NoArgsConstructor;
+
+import java.util.UUID;
+
+@Entity
+@Table(name = "users")
+@NoArgsConstructor
+@Data
+public class User {
+
+    @Id
+    private UUID id;
+
+    @Column
+    private String email;
+
+    @Column
+    private String name;
+
+    @Column
+    private String oauthProvider;
+
+    @Column
+    private String oauthId;
+
+    @Enumerated(value = EnumType.STRING)
+    private Role role;
+
+}
---
+++ db/DDL.sql
@@ -0,0 +1,12 @@
+DROP SCHEMA IF EXISTS jtv_candles CASCADE;
+CREATE SCHEMA jtv_candles;
+
+DROP TABLE IF EXISTS jtv_candles.users CASCADE;
+CREATE TABLE jtv_candles.users (
+                                 id UUID PRIMARY KEY,
+                                 email VARCHAR(255) NOT NULL,
+                                 name VARCHAR(255),
+                                 oauth_provider VARCHAR(50),
+                                 oauth_id VARCHAR(255),
+                                 role VARCHAR(50)
+);
