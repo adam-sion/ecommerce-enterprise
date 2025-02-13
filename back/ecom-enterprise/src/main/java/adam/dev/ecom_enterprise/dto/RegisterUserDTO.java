adbaa8720ff1ebb0c76da79a893157222c85bdf3
adam-sion <adamsion74@gmail.com>
2025-02-13 21:27:22 +0200

    feat: minimal email auth logic

--- back/ecom-enterprise/src/main/java/adam/dev/ecom_enterprise/dto/RegisterUserDTO.java
+++ back/ecom-enterprise/src/main/java/adam/dev/ecom_enterprise/dto/RegisterUserDTO.java
@@ -1,6 +1,5 @@
 package adam.dev.ecom_enterprise.dto;
 
-import com.fasterxml.jackson.annotation.JsonProperty;
 import jakarta.validation.constraints.NotNull;
 import lombok.Data;
 
@@ -13,7 +12,6 @@ public class RegisterUserDTO {
     @NotNull(message = "can't be null")
     private String email;
 
-    @JsonProperty(access = JsonProperty.Access.WRITE_ONLY)
     @NotNull(message = "can't be null")
     private String password;
 }
