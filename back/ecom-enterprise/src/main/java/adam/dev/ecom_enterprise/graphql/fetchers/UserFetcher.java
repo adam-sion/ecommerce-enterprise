b03d890d8fc7c3ec08696d232892374b51a41bfe
adam-sion <adamsion74@gmail.com>
2025-02-14 13:41:41 +0200

    feat: user mapper

--- back/ecom-enterprise/src/main/java/adam/dev/ecom_enterprise/graphql/fetchers/UserFetcher.java
+++ back/ecom-enterprise/src/main/java/adam/dev/ecom_enterprise/graphql/fetchers/UserFetcher.java
@@ -2,11 +2,11 @@ package adam.dev.ecom_enterprise.graphql.fetchers;
 
 import adam.dev.ecom_enterprise.dto.ResponseUserDTO;
 import adam.dev.ecom_enterprise.entity.User;
+import adam.dev.ecom_enterprise.graphql.mappers.UserMapper;
 import adam.dev.ecom_enterprise.service.UserService;
 import com.netflix.graphql.dgs.DgsComponent;
 import com.netflix.graphql.dgs.DgsQuery;
 import lombok.RequiredArgsConstructor;
-import org.springframework.security.core.annotation.AuthenticationPrincipal;
 import org.springframework.security.core.context.SecurityContextHolder;
 import org.springframework.security.core.userdetails.UserDetails;
 
@@ -16,14 +16,14 @@ public class UserFetcher {
 
     private final UserService userService;
 
+    private final UserMapper userMapper;
+
     @DgsQuery
     public ResponseUserDTO me() {
         UserDetails userDetails = (UserDetails) SecurityContextHolder.getContext().getAuthentication().getPrincipal();
         User user = userService.findUserByUsernameOrEmail(userDetails.getUsername())
                 .orElseThrow(() -> new RuntimeException("User not found"));
-        ResponseUserDTO userDTO = new ResponseUserDTO(user.getName(), user.getEmail());
-
-        return userDTO;
+        return userMapper.toResponseUserDTO(user);
     }
 
 }
